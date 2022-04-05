094492ed445be8ae44d9473be145faf5cdcfbd26
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-04-05 09:43:16 +0530

    datatypes

---
+++ JS/Part1/Basics/one.js
@@ -0,0 +1,5 @@
+var ename="Rahul Gandhi";
+//var 9eid=101;
+//var esal@=45000;
+var emp_Id=450;
+console.log(emp_Id);
---
+++ JS/Part1/Basics/product.js
@@ -0,0 +1,21 @@
+//data types - single line 
+/*
+   one 
+   two
+   three
+*/
+var brand="apple";
+var price = 59999;
+var rating = 4.6;
+var discout = true;
+var is_Stock;
+var color=['G','Y','B'];
+var emp = { id:101, name:"Rahul Gandhi"}
+
+console.log(typeof brand);
+console.log(typeof price);
+console.log(typeof rating);
+console.log(typeof discout);
+console.log(typeof is_Stock);
+console.log(typeof color);
+console.log(typeof emp);
