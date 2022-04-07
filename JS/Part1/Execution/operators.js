9a128a116c39325bf13101d4a07c8400f7be13ae
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-04-07 09:35:59 +0530

    okie

---
+++ JS/Part1/Execution/operators.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 20;
+let c = "Hello"
+let d = "30";
+let e;
+console.log(a * b);//200
+console.log(a * c);//NAN
+console.log(a * d);//300
+console.log(a * e);
+/* console.log(a + b)//30
+console.log(a + c)//10Hello
+console.log(a + d)//1030
+console.log(a + e);//NAN */
