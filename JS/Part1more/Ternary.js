db2c88f0e2d156f60be43f82c8de3af732cf4080
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-04-11 09:49:23 +0530

    okie

---
+++ JS/Part1more/Ternary.js
@@ -0,0 +1,2 @@
+let marks = 15;
+marks >= 35 ? console.log("Pass") : console.log("Fail")
---
+++ JS/Part1more/one.js
@@ -0,0 +1,7 @@
+var marks = 28;
+if (marks >= 35) {
+    console.log("Pass")
+}
+else {
+    console.log("Fail")
+}
