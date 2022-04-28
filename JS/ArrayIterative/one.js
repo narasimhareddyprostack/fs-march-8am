cb9fe7b627b3b61d94baad86a52faff35e9cb5a2
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-04-28 10:07:21 +0530

    okie

---
+++ JS/ArrayIterative/one.js
@@ -0,0 +1,5 @@
+let a = [10, 20, 30, 40]
+for (let i = 0; i <= a.length - 1; i++) {
+    //console.log("Hello,GM")
+    console.log(a[i])
+}
---
+++ JS/ArrayIterative/two.js
@@ -0,0 +1,5 @@
+let a = [{ id: 101 }, { id: 102 }, { id: 103 }, { id: 104 }]
+for (let i = 0; i <= a.length - 1; i++) {
+    //console.log(a[i].id)
+    console.log(`Employee ID: ${a[i].id}`)
+}
